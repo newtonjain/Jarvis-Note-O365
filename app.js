289539082658aa5bbdd3fa23b32764fff40ae780
Newton Jain <newton.jain@gmail.com>
2017-07-26 17:39:33 -0700

    Polling every 3 seconds

--- app.js
+++ app.js
@@ -11,46 +11,71 @@
   Office.initialize = function (reason) {
     $(document).ready(function () {
       $('#run').click(run);
+
+    setInterval(run, 3000);
+
     });
   };
 
   function run() {
     
     return OneNote.run(function (context) {
+      console.log('/////////////');
+    // Get the collection of pageContent items from the page.
+    var pageContents = context.application.getActivePage().contents;
 
-            // Get the collection of pageContent items from the page.
-            var pageContents = context.application.getActivePage().contents;
+    // Get the first PageContent on the page, and then get its outline's paragraphs.
+    var outlinePageContents = [];
+    var paragraphs = [];
+    var richTextParagraphs = [];
+    // Queue a command to load the id and type of each page content in the outline.
+    pageContents.load("id,type");
 
-            // Get the first PageContent on the page
-            // Assuming its an outline, get the outline's paragraphs.
-            var pageContent = pageContents.getItemAt(0);
-            var paragraphs = pageContent.outline.paragraphs;
-            var firstParagraph = paragraphs.getItemAt(0);
+    // Run the queued commands, and return a promise to indicate task completion.
+    return context.sync()
+        .then(function () {
+            // Load all page contents of type Outline
+            $.each(pageContents.items, function(index, pageContent) {
+                if(pageContent.type == 'Outline')
+                {
+                    pageContent.load('outline,outline/paragraphs,outline/paragraphs/type');
+                    outlinePageContents.push(pageContent);
+                }
+            });
+            return context.sync();
+        })
+        .then(function () {
+            // Load all rich text paragraphs across outlines
+            $.each(outlinePageContents, function(index, outlinePageContent) {
+                var outline = outlinePageContent.outline;
+                paragraphs = paragraphs.concat(outline.paragraphs.items);
+            });
+            $.each(paragraphs, function(index, paragraph) {
+                if(paragraph.type == 'RichText')
+                {
+                    richTextParagraphs.push(paragraph);
+                    paragraph.load("id,richText/text");
+                }
+            });
+            return context.sync();
+        })
+        .then(function () {
+            // Display all rich text paragraphs to the console
+            $.each(richTextParagraphs, function(index, richTextParagraph) {
+                var richText = richTextParagraph.richText;
+                console.log("Paragraph found with richtext content : " + richText.text);
 
-            // Queue a command to load the id and type of the first paragraph
-            firstParagraph.load("id,type");
+                
+            });
+            return context.sync();
+        });
+}).catch(function(error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+        console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+});  
 
-          // do stuff to the text everytime it changes
-            return context.sync()
-                .then(function () {
-
-                    // Queue commands to insert before and after the first paragraph
-                    firstParagraph.insertRichTextAsSibling("Before", "Text Appears Before Paragraph");
-                    firstParagraph.insertRichTextAsSibling("After", "Text Appears After Paragraph");
-                    
-                    // Run the command to insert text contents
-                    return context.sync();
-                });
-
-            // Run the queued commands, and return a promise to indicate task completion.
-          
-        })  
-        .catch(function(error) {
-            console.log("Error: " + error);
-            if (error instanceof OfficeExtension.Error) {
-                console.log("Debug info: " + JSON.stringify(error.debugInfo));
-            }
-        }); 
   }
 
 })();
